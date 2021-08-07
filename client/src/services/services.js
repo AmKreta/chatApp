f383579a652544c58d105d3ddb56be13499c3a92
AmKreta <41804653+AmKreta@users.noreply.github.com>
2021-08-07 11:35:32 +0530

    Update services.js

--- client/src/services/services.js
+++ client/src/services/services.js
@@ -1,7 +1,7 @@
 export const server = 'http://localhost:8000';
 
 //emoji routes
-const openEmojiApiKey = `d14ba6d429746157a8035ff475b6bc925a71dcae`;
+const openEmojiApiKey = `OPEN_EMOJI_API_KEY_HERE`;
 export const getEmojiCategories = `https://emoji-api.com/categories?access_key=${openEmojiApiKey}`;
 export const getEmojis = (category) => `https://emoji-api.com/categories/${category}?access_key=${openEmojiApiKey}`;
 export const searchEmoji = (query) => `https://emoji-api.com/emojis?search=${query}&access_key=${openEmojiApiKey}`;
@@ -9,7 +9,7 @@ export const getAllEmojis = `https://emoji-api.com/emojis?access_key=${openEmoji
 
 
 //giphy toutes
-const giphyApiKey = `n0gTMxbskDas7UI5DeAYMMHNR0aPCzqq`;
+const giphyApiKey = `GIPHY_API_KEY_HERE`;
 export const getGif = ({ query, page }) => {
     const url = query === ''
         ? `https://api.giphy.com/v1/gifs/trending?api_key=${giphyApiKey}&limit=30&rating=g`
