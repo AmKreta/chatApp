8e8d2fa320bab363867c028dadb6a4f84274fdf4
Amresh Kumar <41804653+AmKreta@users.noreply.github.com>
2024-05-19 18:20:56 +0530

    added init times ago

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import TimeAgo from 'javascript-time-ago'
+import en from 'javascript-time-ago/locale/en'
+TimeAgo.addDefaultLocale(en);
+
+const timeout = setTimeout(function(){
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+  clearTimeout(timeout);
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
